8a82603a83b887634f23287f6f2167e761568792
zzhoho0110 <abhoho0110@gmail.com>
2024-07-11 03:43:49 +0800

    Update buildImage.groovy, buildJar.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t zzhoho0110/java-maven-app:jma-1.1.2 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push zzhoho0110/java-maven-app:jma-1.1.2'
+        sh "docker push $imageName"
     }
 }
